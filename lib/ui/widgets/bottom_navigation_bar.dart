74dedb0b7513720d486cb5d24e12468192c5e9ee
Marco Osorio <marco.osorio86@hotmail.com>
2025-02-17 07:13:25 -0500

    Se crea una segunda opción de ButtonNavBar

--- lib/ui/widgets/bottom_navigation_bar.dart
+++ lib/ui/widgets/bottom_navigation_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lingerie_store_project/layout/colors.dart';
 
 /// La barra inferior es dinámica.
 /// Primero debe ser persistente, aunque suene contradictorio, en toda la aplicación.
@@ -18,78 +19,78 @@ class CustomBottomNavigationBar extends StatefulWidget {
 
 class _CustomBottomNavigationBar extends State<CustomBottomNavigationBar> {
   @override
-  // Widget build(BuildContext context) {
-  //   return BottomNavigationBar(
-  //     enableFeedback: true,
-  //     backgroundColor: BrandColors.appBarColor.value,
-  //     selectedItemColor: Colors.black87,
-  //     unselectedItemColor: Colors.black45,
-  //     currentIndex: widget.selectedIndex,
-  //     onTap: widget.onItemTapped,
-  //     // [fixed] Permite mostrar todos los [Icon]s y su [label] al mismo tiempo.
-  //     // [shifting] Muestra todos los [Icon]s, pero solo el [label] del que esté seleccionado.
-  //     type: BottomNavigationBarType.shifting,
-  //     selectedLabelStyle: TextStyle(fontWeight: FontWeight.w500),
-  //     showSelectedLabels: true,
-  //     items: [
-  //       _buildNavItem(Icons.home_rounded, 'Home', 0),
-  //       _buildNavItem(Icons.shopping_bag_rounded, 'Products', 1),
-  //       _buildNavItem(Icons.shopping_cart_rounded, 'Cart', 2),
-  //       _buildNavItem(Icons.person_2_rounded, 'Profile', 3),
-  //     ],
-  //   );
-  // }
-
-  // BottomNavigationBarItem _buildNavItem(
-  //     IconData icon, String label, int index) {
-  //   return BottomNavigationBarItem(
-  //     icon: Icon(icon),
-  //     label: label,
-  //     tooltip: label,
-  //   );
-  // }
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-          bottom: 5, right: 10, left: 10), // Lifts the BottomAppBar
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(30), // Rounded corners
-        // boxShadow: [
-        //   BoxShadow(
-        //     color: Colors.black26,
-        //     blurRadius: 10, // Soft shadow
-        //     spreadRadius: 2,
-        //     offset: Offset(0, 4), // Shadow below
-        //   ),
-        // ],
-      ),
-      child: BottomAppBar(
-        // color: BrandColors.transparent.value,
-        shape: CircularNotchedRectangle(), // For FloatingActionButton notch
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            _buildNavItem(Icons.home_rounded, 'Home', 0),
-            _buildNavItem(Icons.shopping_bag_rounded, 'Products', 1),
-            _buildNavItem(Icons.shopping_cart_rounded, 'Cart', 2),
-            _buildNavItem(Icons.person_2_rounded, 'Profile', 3),
-          ],
-        ),
-      ),
+    return BottomNavigationBar(
+      enableFeedback: true,
+      backgroundColor: BrandColors.appBarColor.value,
+      selectedItemColor: Colors.black87,
+      unselectedItemColor: Colors.black45,
+      currentIndex: widget.selectedIndex,
+      onTap: widget.onItemTapped,
+      // [fixed] Permite mostrar todos los [Icon]s y su [label] al mismo tiempo.
+      // [shifting] Muestra todos los [Icon]s, pero solo el [label] del que esté seleccionado.
+      type: BottomNavigationBarType.shifting,
+      selectedLabelStyle: TextStyle(fontWeight: FontWeight.w500),
+      showSelectedLabels: true,
+      items: [
+        _buildNavItem(Icons.home_rounded, 'Home', 0),
+        _buildNavItem(Icons.shopping_bag_rounded, 'Products', 1),
+        _buildNavItem(Icons.shopping_cart_rounded, 'Cart', 2),
+        _buildNavItem(Icons.person_2_rounded, 'Profile', 3),
+      ],
     );
   }
 
-  Widget _buildNavItem(IconData icon, String label, int index) {
-    return Expanded(
-      child: IconButton(
-        icon: Icon(icon,
-            color: widget.selectedIndex == index
-                ? Colors.black87
-                : Colors.black45),
-        tooltip: label,
-        onPressed: () => widget.onItemTapped(index),
-      ),
+  BottomNavigationBarItem _buildNavItem(
+      IconData icon, String label, int index) {
+    return BottomNavigationBarItem(
+      icon: Icon(icon),
+      label: label,
+      tooltip: label,
     );
   }
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     margin: EdgeInsets.only(
+  //         bottom: 5, right: 10, left: 10), // Lifts the BottomAppBar
+  //     decoration: BoxDecoration(
+  //       color: Colors.white,
+  //       borderRadius: BorderRadius.circular(30), // Rounded corners
+  //       // boxShadow: [
+  //       //   BoxShadow(
+  //       //     color: Colors.black26,
+  //       //     blurRadius: 10, // Soft shadow
+  //       //     spreadRadius: 2,
+  //       //     offset: Offset(0, 4), // Shadow below
+  //       //   ),
+  //       // ],
+  //     ),
+  //     child: BottomAppBar(
+  //       color: BrandColors.transparent.value,
+  //       shape: CircularNotchedRectangle(), // For FloatingActionButton notch
+  //       child: Row(
+  //         mainAxisAlignment: MainAxisAlignment.spaceAround,
+  //         children: [
+  //           _buildNavItem(Icons.home_rounded, 'Home', 0),
+  //           _buildNavItem(Icons.shopping_bag_rounded, 'Products', 1),
+  //           _buildNavItem(Icons.shopping_cart_rounded, 'Cart', 2),
+  //           _buildNavItem(Icons.person_2_rounded, 'Profile', 3),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  // Widget _buildNavItem(IconData icon, String label, int index) {
+  //   return Expanded(
+  //     child: IconButton(
+  //       icon: Icon(icon,
+  //           color: widget.selectedIndex == index
+  //               ? Colors.black87
+  //               : Colors.black45),
+  //       tooltip: label,
+  //       onPressed: () => widget.onItemTapped(index),
+  //     ),
+  //   );
+  // }
 }
